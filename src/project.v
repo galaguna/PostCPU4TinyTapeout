03e62ada27e764d97509002d49c6bf48e3d3ae92
Gerardo Abel Laguna-Sanchez <65410505+galaguna@users.noreply.github.com>
2025-08-01 14:51:18 -0600

    Add files via upload

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Gerardo Laguna-Sanchez
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_PostSys (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,51 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    //signals:
+    wire loc_clk;
+    wire loc_NRst, loc_PRst; 
+    wire mode; 
+    wire exec;
+    wire [2:0] out_ctrl;
+    wire spi_sck, spi_mosi, spi_cs, spi_miso;
+    
+    wire [3:0] state_nibble;
+    wire [7:0] out8b;
+    wire [2:0] out3b;
+    
+    //instantiations:
+    Post_sys_4Tiny my_PostSys
+    (
+    .CLK(loc_clk), .NRST(loc_NRst),
+    .RUN(exec), .MODE(mode), .OUT_CTRL(out_ctrl),
+    .STATE(state_nibble),
+    .OUT8B(out8b), .OUT3B(out3b),
+    .SPI_CS(spi_cs), .SPI_MOSI(spi_mosi), .SPI_SCK(spi_sck),
+    .SPI_MISO(spi_miso)
+    );    
+    
+  // interconnection logic:
+    assign loc_clk = clk; //1525.879 Hz
+    assign loc_NRst = rst_n;
+    assign loc_PRst = ~loc_NRst;
+
+    assign out_ctrl = ui_in[2:0];
+    assign spi_sck  = ui_in[3];
+    assign spi_mosi = ui_in[4];
+    assign spi_cs   = ui_in[5];
+    assign exec     = ui_in[6];
+    assign mode     = ui_in[7];
+
+    assign state_nibble = uio_out[3:0];
+    assign out3b        = uio_out[6:4];
+    assign spi_miso     = uio_out[7];
+
+    assign out8b        = uo_out;
+
+  //output logic:
+  assign uio_oe  = 8'b11111111;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
